550a2264db42a7278256c661db55237623b0e026
aetos <aetos@aerie.jp>
2017-12-19 12:50:23 +0900

    こまごま

--- hex_manip.h
+++ hex_manip.h
@@ -4,8 +4,8 @@
 
 struct hex_value
 {
-	const unsigned int width;
 	const unsigned int value;
+	const unsigned int width;
 };
 
 std::ostream & operator <<(std::ostream & out, hex_value const & bit)
@@ -13,28 +13,28 @@ std::ostream & operator <<(std::ostream & out, hex_value const & bit)
 	return out << " (0x" << std::setw(bit.width) << std::setfill('0') << std::hex << bit.value << ")";
 }
 
-hex_value hex(unsigned int width, unsigned int value)
+hex_value hex(unsigned int value, unsigned int width)
 {
-	hex_value bit = { width, value };
+	hex_value bit = { value, width };
 	return bit;
 }
 
 hex_value hex(unsigned char value)
 {
-	return hex(2, value);
+	return hex(value, 2);
 }
 
 hex_value hex(unsigned short value)
 {
-	return hex(4, value);
+	return hex(value, 4);
 }
 
 hex_value hex(unsigned int value)
 {
-	return hex(8, value);
+	return hex(value, 8);
 }
 
 hex_value hex(unsigned long value)
 {
-	return hex(8, value);
+	return hex(value, 8);
 }
